e0df6d2e2e01098bf59ecd6915f9b32de1182312
root <root@localhost.localdomain>
2018-02-05 07:46:50 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,318 @@
+/*
+.row1 {
+background-color:#b3edff;
+}
+*/
+var storage = window.localStorage;
+var value_date = storage.getItem("date");
+var result = [];
+var rowName = [];
+var answer = [];
+var answer1 = [];
+var one_value = [];
+var two_value = [];
+var gau = ['CitriCure', 'Guazalil SL'];
+var ima = ['Guazalil SL','ImazaCure 500 EC','ImaCulate 300 EC', 'ImazaCure 750 SG'];
+var tbz = ['ICA Thiabendazole 500 SC'];
+var proc = ['ICA-Prochloraz 450 EC'];
+var pyr = ['Protector 400 SC', 'Propirly 270 EC'];
+var prop = ['PropiCure 250 EC', 'Propirly 270 EC'];
+var flud = ['Teacher 230 SC'];
+var ddac = ['Sporekill'];
+var dbx;
+var data1;
+var date_file = null;
+var boolean_row = true;
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// device APIs are available
+//
+function nothing_func() {
+ return false;
+}
+function alert_uncheck_za() {
+  navigator.notification.alert("Residues must comply to import as well as export countries (RSA) MRL's.",nothing_func,'Note', 'Done');
+}
+function alert_desc() {
+navigator.notification.alert("ICA citrus post-harvest fungicide choices for different export markets.\n\nNote that compliance with product label instructions will ensure that residues do not exceed RSA MRL, but may not meet the import requirements of countries when MRL’s are lower than in RSA.",nothing_func,'Note', 'Done');
+}
+function onDeviceReady() {
+    $( '#gallery' ).click( function( e ) {
+				e.preventDefault();
+				$.swipebox( [ 
+					{ href : 'labels/CitruCure.jpg', title : 'Full labels on ICA website.' },
+					{ href : 'labels/Imaculate300EC.jpg', title : 'Full labels on ICA website.' },
+                                        { href : 'labels/ImazaCure500EC.jpg', title : 'Full labels on ICA website.' },
+                                        { href : 'labels/PropiCureLabel.jpg', title : 'Full labels on ICA website.' },
+					{ href : 'labels/PropirlyLabel.jpg', title : 'Full labels on ICA website.' },
+                                        { href : 'labels/Protector400SC.jpg', title : 'Full labels on ICA website.' },
+                                        { href : 'labels/Sporekill.jpg', title : 'Full labels on ICA website.' },
+					{ href : 'labels/TEACHER.jpg', title : 'Full labels on ICA website.' },
+                                        { href : 'labels/Thiabendazole500SC.jpg', title : 'Full labels on ICA website.' }
+				], {hideBarsDelay : 0, removeBarsOnMobile: false, loopAtEnd: true});
+			} );
+var sCSV = "c,RSA,EU,USA,Codex A,Codex B,Japan,Korea,Canada,Taiwan,GSO,Hong Kong\nLemons,DDAC 6;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 2;Propiconazole 6;Pyrimethanil 10;TBZ 6,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz 10;Propiconazole 5;Pyrimethanil 8;TBZ 7,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 10;Prochloraz N;Propiconazole 8;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole N;Pyrimethanil 7;TBZ 7,DDAC 6;Fludioxonil 7;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole 6;Pyrimethanil 10;TBZ 5,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole N;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 5;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole 6;Pyrimethanil 7;TBZ 10,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole 8;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 7;Guazatine 5;Imazalil 5;Prochloraz N;Propiconazole 4;Pyrimethanil 7;TBZ 10,DDAC N;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole N;Pyrimethanil 7;TBZ 7,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 10;Prochloraz 10;Propiconazole N;Pyrimethanil 10;TBZ 10\nGrapefruit,DDAC 6;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 2;Propiconazole 6;Pyrimethanil 10;TBZ 6,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz 10;Propiconazole 5;Pyrimethanil 8;TBZ 7,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 10;Prochloraz N;Propiconazole 8;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole N;Pyrimethanil 7;TBZ 7,DDAC 6;Fludioxonil 7;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole 6;Pyrimethanil 10;TBZ 5,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole N;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole 4;Pyrimethanil 7;TBZ 10,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole 8;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 5;Guazatine 5;Imazalil 5;Prochloraz N;Propiconazole 4;Pyrimethanil 7;TBZ 10,DDAC N;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole N;Pyrimethanil 7;TBZ 7,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 10;Prochloraz 10;Propiconazole N;Pyrimethanil 10;TBZ 10\nOranges,DDAC 6;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 2;Propiconazole 6;Pyrimethanil 10;TBZ 6,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz 10;Propiconazole 9;Pyrimethanil 8;TBZ 7,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 10;Prochloraz N;Propiconazole 8;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole 9;Pyrimethanil 7;TBZ 7,DDAC 6;Fludioxonil 7;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole 6;Pyrimethanil 10;TBZ 5,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole N;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 5;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole 8;Pyrimethanil 7;TBZ 10,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole 8;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 5;Guazatine 5;Imazalil 5;Prochloraz N;Propiconazole 4;Pyrimethanil 7;TBZ 10,DDAC N;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole 9;Pyrimethanil 7;TBZ 7,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 10;Prochloraz 10;Propiconazole N;Pyrimethanil 10;TBZ 10\nMandarin types,DDAC 6;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 2;Propiconazole 6;Pyrimethanil 10;TBZ 6,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz 10;Propiconazole 5;Pyrimethanil 8;TBZ 7,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 10;Prochloraz N;Propiconazole 8;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole N;Pyrimethanil 7;TBZ 7,DDAC 6;Fludioxonil 7;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole 6;Pyrimethanil 10;TBZ 5,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole N;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 1;Guazatine N;Imazalil 5;Prochloraz 1;Propiconazole N;Pyrimethanil 1;TBZ 10,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 5;Prochloraz N;Propiconazole 8;Pyrimethanil 10;TBZ 10,DDAC N;Fludioxonil 7;Guazatine 5;Imazalil 5;Prochloraz N;Propiconazole N;Pyrimethanil 7;TBZ 10,DDAC N;Fludioxonil 10;Guazatine 5;Imazalil 5;Prochloraz 10;Propiconazole N;Pyrimethanil 7;TBZ 7,DDAC N;Fludioxonil 10;Guazatine N;Imazalil 10;Prochloraz 10;Propiconazole N;Pyrimethanil 10;TBZ 10";
+	var lines=sCSV.split("\n");
+
+	var headers=lines[0].split(",");
+	for(var i=1;i<lines.length;i++){
+
+	  var obj = {};
+	  var currentline=lines[i].split(",");
+	  rowName[i-1] = currentline[0];
+	  if (currentline[0] != "") {
+	  	$("#checkbox_div").append(currentline[0]+"<input class='name' name='citrus_rad' type='radio' id='"+i+"'><br>");
+	  }
+	  for(var j=1;j<headers.length;j++){
+		  if (i == 1) {
+                if (headers[j] == 'RSA') {
+                       var country_name_text = 'RSA';
+                       var selected = country_name_text;
+                       $("#checkbox_div2").append("<div class='country_name' style='position: relative' id='"+headers[j].toLowerCase()+"_con'>"+headers[j]+"<input class='input_con_name' type='checkbox' checked='true' data-country-name='"+headers[j]+"' id='"+headers[j]+"'></div>");
+                       var string_html0 = '<div id="'+selected+'_sel"> '+ country_name_text +' <input type="checkbox" checked="true"  class="input_con_name_sel" data-country-name0 = "'+country_name_text +'"/> </div>';
+        $("#check_countries").append(string_html0);
+                } else {
+	  	       $("#checkbox_div2").append("<div class='country_name' style='position: relative' id='"+headers[j].toLowerCase()+"_con'>"+headers[j]+"<input class='input_con_name' type='checkbox' data-country-name='"+headers[j]+"' id='"+headers[j]+"'></div>");
+                }
+	  	}
+		  obj[headers[j]] = currentline[j];
+
+	  }
+	  result.push(obj);
+
+	}
+
+	//result.pop();
+	var sCSV = "Codex A,Angola,Benin,Botswana,Congo,Gabon,Kenya,Madagascar,Mali,Mauritius,Mauritania,Namibia,Senegal,Seychelles,Reunion,Sudan,Tanzania,China,Indonesia,Malaysia,Philippines,Singapore,Vietnam\nCodex B,Burkina Faso,Cameroon,Côte d'Ivoire,Malawi,Nigeria,Tunisia,Uganda,Bangladesh,Sri Lanka,Iran,Kuwait,Pakistan,Azerbaijan,Jordan,Georgia,Russia\nGSO,United Arab Emirates,Bahrain,Saudi Arabia,Oman,Qatar,Kuwait,Yemen";
+	var lines=sCSV.split("\n");
+	for(var i=0;i<lines.length;i++){
+		var currentline=lines[i].split(",");
+		for(var j=1;j<currentline.length;j++){
+			if (currentline[j] != "") {
+  				$("#checkbox_div2").append("<div class='country_name' style='position: relative' id='"+currentline[j].toLowerCase()+"_con'>"+currentline[j]+"<input class='input_con_name' data-country-name='"+currentline[j]+"' type='checkbox' id='"+currentline[0]+"'></div>");
+			}
+  		}
+		
+	}
+$('#checkbox_div2 div').sort(function (a, b)
+	{
+		return String.prototype.localeCompare.call(a.id.toLowerCase(), b.id.toLowerCase());
+	}
+).each(function() {
+	var elem = $(this);
+	elem.remove();
+	$(elem).appendTo('#checkbox_div2');
+});
+
+  jQuery.fn.scrollTo = function(elem) { 
+    $(this).scrollTop($(this).scrollTop() - $(this).offset().top + $(elem).offset().top); 
+    return this; 
+};
+$(".input_con_name").change(function () {
+    var country_name_text = $(this).data('country-name');
+    var selected = country_name_text.split(' ').join('_');
+    if ($('#' + selected + '_sel').length == 0) {
+        var string_html0 = '<div id="'+selected+'_sel"> '+ country_name_text +' <input type="checkbox" checked="true"  class="input_con_name_sel" data-country-name0 = "'+country_name_text +'"/> </div>';
+        $("#check_countries").append(string_html0);
+    } else {
+        if (country_name_text == 'RSA') {
+            alert_uncheck_za();
+        }
+        $('#' + selected + '_sel').remove();
+    }
+    $(".input_con_name_sel").change(function () {
+        var country_name_text0 = $(this).data('country-name0');
+        if (country_name_text0 == 'RSA') {
+            alert_uncheck_za();
+        }
+        var selected0 = country_name_text0.split(' ').join('_');
+        $('*[data-country-name="'+country_name_text0+'"]').prop('checked', false);
+        $('#' + selected0 + '_sel').remove();
+    });
+});
+$(".input_con_name_sel").change(function () {
+        var country_name_text0 = $(this).data('country-name0');
+        if (country_name_text0 == 'RSA') {
+            alert_uncheck_za();
+        }
+        var selected0 = country_name_text0.split(' ').join('_');
+        $('*[data-country-name="'+country_name_text0+'"]').prop('checked', false);
+        $('#' + selected0 + '_sel').remove();
+    });
+    }
+    // alert dialog dismissed
+    function alertDismissed() {
+            // do something
+    }
+
+    // Show a custom alertDismissed
+    //
+    function showAlert() {
+        /*navigator.notification.alert(
+            'You are the winner!',  // message
+            alertDismissed,         // callback
+            'Game Over',            // title
+            'Done'                  // buttonName
+        );
+	$('#test_d').append("test");*/
+    }
+    function go_to_div(div_go_to) {
+        $(document).ready(function () {
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $('#'+div_go_to).offset().top
+    }, 'slow');
+});
+    }
+    function get_Values() {
+	$("#answer").empty();
+        var string_ans = '';
+	$("#countries_selected").empty();
+        var citrus_name = '';
+	var checked_countries= [];
+	var checked_citrus= [];
+	var arrNoM = [];
+	one_value = [];
+	two_value = [];
+	answer = [];
+
+	$("#checkbox_div2 input:checked").each(function(){
+		$("#countries_selected").append($(this).data('country-name')+", ");
+		checked_countries.push($(this).attr('id'));	
+	});
+	$("#checkbox_div input:checked").each(function(){
+                citrus_name = rowName[$(this).attr('id')-1]
+		checked_citrus.push($(this).attr('id')-1);	
+	});
+	var htmlwrong = document.getElementById("countries_selected").innerHTML;
+        document.getElementById("countries_selected").innerHTML = htmlwrong.substr(0, htmlwrong.length-2);
+        document.getElementById("countries_selected").innerHTML += "<br><b>Citrus Commodity</b>: " + citrus_name;
+	//console.log(checked_countries);
+	//console.log(checked_citrus);
+	for (var i = 0; i < checked_countries.length; i++) {
+		for (var j = 0; j < checked_citrus.length; j++) {
+			add_to_answer(result[parseInt(checked_citrus[j])][checked_countries[i]]);
+		}
+	}
+	for (var i = 0; i < answer.length; i++) {
+		var two_values = [];
+		
+		two_values = answer[i].split(" ");
+		if (one_value.indexOf(two_values[0]) == -1) {
+			if(two_values[1] == "N") {
+				one_value[i] = two_values[0];
+				two_value[i] = parseFloat("-1");
+			} else if (two_values[1] == "Exempt") {
+				one_value[i] = two_values[0];
+				two_value[i] = parseFloat("-2");
+			} else {
+				one_value[i] = two_values[0];
+				two_value[i] = parseFloat(two_values[1]);
+			}
+		} else {
+			var index_val = one_value.indexOf(two_values[0]);
+			var value = two_value[index_val];
+			if(two_values[1] == "N") {
+				two_value[index_val] = parseFloat("-1");
+			} else if (two_values[1] == "E") {
+				if (value == -1) {
+				}  else {
+					two_value[index_val] = parseFloat("-2");
+				}
+			} else {
+				if (value == -2) {
+					two_value[index_val] = parseFloat("-2");
+				}  else if (value == -1) {
+					two_value[index_val] = parseFloat("-1");
+				} else if (parseFloat(two_values[1]) < value) {
+					two_value[index_val] = parseFloat(two_values[1]);
+				}
+			}
+		}
+		
+	}
+        string_ans = "<table id='mymrltable'>";
+        string_ans += "<tr><th style='text-align:left;'>Active </th><th>MRL</th><th style='text-align:left;'>Products</th> </tr>";
+	var str = '';
+        var arrProducts = [];
+        var row = '';
+	for (var i = 0; i < one_value.length; i++) {
+                
+		str = '';
+                if (one_value[i] == "DDAC") {
+			arrProducts = ddac;
+                } else if (one_value[i] == "Fludioxonil") {
+			arrProducts = flud;
+		} else if (one_value[i] == "Guazatine") {
+			arrProducts = gau;
+		} else if (one_value[i] == "Imazalil") {
+			arrProducts = ima;
+		} else if (one_value[i] == "Prochloraz") {
+			arrProducts = proc;
+		} else if (one_value[i] == "Propiconazole") {
+			arrProducts = prop;
+		} else if (one_value[i] == "Pyrimethanil") {
+			arrProducts = pyr;
+		} else if (one_value[i] == "TBZ") {
+			arrProducts = tbz;
+		}
+                for (var j = 0; j < arrProducts.length; j++) {
+			str += "<div id ="+arrProducts[j].split(' ').join('_')+">" + arrProducts[j] + "</div>";
+                }
+		if (two_value[i] == -1) {
+			//$("#answer").append(one_value[i]+ " N <br>");
+			arrNoM = $.merge(arrNoM, arrProducts);
+			
+		} else if (two_value[i] == -2) {
+                        if (boolean_row == true) {
+                    row = 'row0';
+                    boolean_row = false;
+                } else {
+                    row = 'row1';
+                    boolean_row = true;
+                }
+			string_ans += "<tr class='"+row+"'> <td>" + one_value[i] + "</td><td align='center'> Exempt </td> <td>"+str+" </td></tr>";
+		} else {
+                        if (boolean_row == true) {
+                    row = 'row0';
+                    boolean_row = false;
+                } else {
+                    row = 'row1';
+                    boolean_row = true;
+                }
+			string_ans +=  "<tr class='"+row+"'> <td>" + one_value[i] + "</td><td align='center'> " + two_value[i] +"</td> <td>"+str+" </td></tr>";
+		}
+	}
+        if (boolean_row == true) {
+                    row = 'row0';
+                    boolean_row = false;
+                } else {
+                    row = 'row1';
+                    boolean_row = true;
+                }
+        string_ans += "<tr class='"+row+"'> <td> Peracetic Acid Peroxide</td><td align='center'> <p style='color:#0095C1;'>*</p> </td> <td> HyperCide </td></tr>";
+	string_ans += "</table></div>";
+        $("#answer").append(string_ans);
+	for (var j = 0; j < arrNoM.length; j++) {
+	    $('#'+arrNoM[j].split(' ').join('_')).remove();
+	}
+	$("#answer").append("<p style='color:red;'> Residues must comply to import as well as export countries (RSA) MRL's.</p><p style='color:#0095C1;'>* No residue</p><p style='color:red;'>No guarantees are given that the MRL data is correct at time of use. It is the user's responsibility to be familiar with the latest MRL requirements of their markets. </p>");
+go_to_div("end_div");
+}
+function add_to_answer(to_add) {
+	var values_add = to_add.split(";");
+	for (var i = 0; i < values_add.length; i++) {
+		if (answer.indexOf(values_add[i]) == -1) {
+			answer.push(values_add[i]);		
+		}
+	}
+}
+function scroll_to() {
+	var $container = $('#checkbox_div2');
+    	$value = $('#input_country').val().toLowerCase();
+        var $scrolltodiv = $('div[id^='+$value+'][id$=_con]');
+	$("#checkbox_div2").scrollTo($scrolltodiv);
+}
+
